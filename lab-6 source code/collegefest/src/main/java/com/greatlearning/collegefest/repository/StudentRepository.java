b72ec18cbd55081d4cee3a0e7284d8c47af64acd
Kpritam719 <110289730+Kpritam719@users.noreply.github.com>
2023-02-09 22:52:28 +0530

    Delete lab-6 source code/collegefest/src/main directory

--- lab-6 source code/collegefest/src/main/java/com/greatlearning/collegefest/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.collegefest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.collegefest.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
